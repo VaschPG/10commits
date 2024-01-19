a1cfd67fef52aaefd33adb5b4a4ff02ce21375da
philippe.guerrier <137076130+VaschPG@users.noreply.github.com>
2024-01-18 19:11:15 -0500

    Premier commit MonServlet

---
+++ src/servlets/MonServlet.java
@@ -0,0 +1,35 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MonServlet", urlPatterns = {"/mon-servlet"})
+public class MonServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Vue 1</title></head>");
+        out.println("<body>");
+        out.println("<h1>C'est la Vue 1</h1>");
+        out.println("<p>C'est quand on fait le doGet</p>");
+        out.println("</body></html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Vue 2</title></head>");
+        out.println("<body>");
+        out.println("<h1>Vue 2</h1>");
+        out.println("<p>C'est quand on fait le doPost</p>");
+        out.println("</body></html>");
+    }
+}
